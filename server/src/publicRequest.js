f2fbd4839b13e4dbea2053ee88f298a6a62bae14
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2023-01-15 17:25:45 +0800

    updated request

--- server/src/publicRequest.js
+++ server/src/publicRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const BASE_URL = "http://191.96.57.97/api/";
 //const BASE_URL = "http://localhost:5000/api"; 
-const BASE_URL = "https://tua.ebenta.admin.onrender.com/api";
+const BASE_URL = "https://tua-ebenta-admin.onrender.com/api";
 
   const user = JSON.parse(localStorage.getItem("persist:root"))?.auth;
   const currentUser = user && JSON.parse(user).currentUser;
